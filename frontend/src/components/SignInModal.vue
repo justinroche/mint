57f1496a09940acc76d1a123b2497a6f22c8c7bc
justinroche <justinroche03@gmail.com>
2024-10-08 13:12:41 -0500

    Improve error handling for sign in modal

--- frontend/src/components/SignInModal.vue
+++ frontend/src/components/SignInModal.vue
@@ -9,16 +9,27 @@ const showModalStore = useShowModalStore();
 const email = ref('');
 const password = ref('');
 
+const showEmailError = ref(false);
+const showPasswordError = ref(false);
+
 const switchToCreateAccountModal = () => {
   showModalStore.showSignInModal = false;
   showModalStore.showCreateAccountModal = true;
 };
 
 const attemptLogin = async () => {
+  showEmailError.value = false;
+  showPasswordError.value = false;
   const error = await performLogin(email.value, password.value);
   if (error) {
-    // TODO: handle error more elegantly
-    alert(error);
+    if (error === 'User not found') {
+      showEmailError.value = true;
+    } else if (error === 'Invalid password') {
+      showPasswordError.value = true;
+    } else {
+      // TODO: handle other errors more elegantly
+      alert(error);
+    }
     return;
   }
   showModalStore.showSignInModal = false;
@@ -33,22 +44,34 @@ const attemptLogin = async () => {
     </div>
     <div class="modal-body">
       <form class="sign-in-form" @submit.prevent="attemptLogin">
-        <input
-          v-model="email"
-          class="form-input"
-          type="email"
-          id="email"
-          placeholder="Email"
-          required
-        />
-        <input
-          v-model="password"
-          class="form-input"
-          type="password"
-          id="password"
-          placeholder="Password"
-          required
-        />
+        <div>
+          <input
+            v-model="email"
+            class="form-input"
+            :class="{ error: showEmailError }"
+            type="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+          <span class="error-span" v-if="showEmailError"
+            >No user found with this email address.</span
+          >
+        </div>
+        <div>
+          <input
+            v-model="password"
+            class="form-input"
+            :class="{ error: showPasswordError }"
+            type="password"
+            id="password"
+            placeholder="Password"
+            required
+          />
+          <span class="error-span" v-if="showPasswordError"
+            >Incorrect password.</span
+          >
+        </div>
         <button class="menu-button" type="submit">Sign in</button>
       </form>
     </div>
@@ -115,5 +138,13 @@ const attemptLogin = async () => {
   display: flex;
   flex-direction: column;
   gap: 25px;
+}
+
+.error-span {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-size: 0.8rem;
+  padding-top: 5px;
 }
 </style>
