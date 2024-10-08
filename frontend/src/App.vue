f3f613f9b83d0c7122333a03bfa9d9f464c1a5cb
justinroche <justinroche03@gmail.com>
2024-10-08 12:21:35 -0500

    Added a little transition for router pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,10 @@ const showModalStore = useShowModalStore();
 
 <template>
   <page-header />
-  <router-view />
+
+  <Transition name="fade">
+    <router-view />
+  </Transition>
 
   <Transition name="modal">
     <create-account-modal v-if="showModalStore.showCreateAccountModal" />
@@ -33,6 +36,21 @@ const showModalStore = useShowModalStore();
 
 .modal-enter-to,
 .modal-leave-from {
+  opacity: 1;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to,
+.fade-leave-from {
   opacity: 1;
 }
 </style>
