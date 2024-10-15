702e9b212c47f39c6fbc13535e5d4014d1ed6102
justinroche <justinroche03@gmail.com>
2024-10-14 20:43:22 -0500

    Fix header transition

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue';
 import PageHeader from './components/PageHeader.vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
+const showHeader = ref(route.path !== '/');
+
+watch(
+  () => route.path,
+  (newPath) => {
+    if (newPath === '/') {
+      showHeader.value = false;
+    } else if (!showHeader.value) {
+      // Delay showing header to allow for route transition
+      setTimeout(() => {
+        showHeader.value = true;
+      }, 500);
+    }
+  }
+);
 </script>
 
 <template>
   <Transition name="fade" mode="out-in">
-    <component :is="PageHeader" v-if="route.path !== '/'" />
+    <PageHeader v-if="showHeader" />
   </Transition>
 
   <router-view v-slot="{ Component }">
